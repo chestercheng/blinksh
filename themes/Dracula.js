3d4e20571cc91077b5329adc63fdb25792989495
chestercheng <chester7864@gmail.com>
2021-05-25 14:44:39 +0800

    feat: first commit

---
+++ themes/Dracula.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#ff5555", "#50fa7b", "#f1fa8c", "#6272a4", "#ff79c6", "#8be9fd", "#f8f8f2", "#676965", "#ff6e6e", "#69ff94", "#ffffa5", "#83afd8", "#ff92df", "#a4ffff", "#f1f1ef"]);
+t.prefs_.set('foreground-color', "#f8f8f2");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(248,248,242,0.5)');
